c4ef7ab53182ab3e18076e0dc2fa7aa1c31ef62f
Ruan Correia <correia.ruan@gmail.com>
2025-09-22 17:08:52 -0300

    Update CtrlAbstrato.java

--- controller/CtrlAbstrato.java
+++ controller/CtrlAbstrato.java
@@ -1,39 +1,17 @@
 package controller;
 
-abstract public class CtrlAbstrato {
-	// 
-	// ATRIBUTOS
-	//
-	
-	/**
-	 * Representa o controlador que disparou a
-	 * execução do controlador em questão
-	 */
-	final private CtrlAbstrato ctrlPai;
-	
-	
-	//
-	// MÉTODOS
-	//
-	public CtrlAbstrato(CtrlAbstrato pai) {
-		this.ctrlPai = pai;
-	}
-	
-	/**
-	 * Retorna quem é o controlador pai que disparou a execução 
-	 * do caso de uso em questão. 
-	 */
-	public CtrlAbstrato getCtrlPai() {
-		return this.ctrlPai;
-	}
-	/**
-	 * Método que define o que é para ser feito ao final do caso de uso
-	 */
-	abstract public void encerrar();
-	
-	/**
-	 * Método que define qual é o bem tangível produzido 
-	 * ao final do caso de uso
-	 */
-	abstract public Object getBemTangivel();	
+public abstract class CtrlAbstrato {
+    private final CtrlAbstrato ctrlPai;
+
+    public CtrlAbstrato(CtrlAbstrato pai) {
+        this.ctrlPai = pai;
+    }
+
+    public CtrlAbstrato getCtrlPai() {
+        return this.ctrlPai;
+    }
+
+    public abstract void encerrar();
+
+    public abstract Object getBemTangivel();
 }
