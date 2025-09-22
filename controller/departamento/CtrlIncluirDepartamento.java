c33f639a24c88f587492d31bd0b8ad856057feec
Ruan Correia <correia.ruan@gmail.com>
2025-09-22 17:10:04 -0300

    Update CtrlIncluirDepartamento.java

--- controller/departamento/CtrlIncluirDepartamento.java
+++ controller/departamento/CtrlIncluirDepartamento.java
@@ -6,22 +6,21 @@ import model.ModelException;
 import model.dao.DaoDepartamento;
 
 public class CtrlIncluirDepartamento extends CtrlAbstratoDepartamento {
-	//
-	// MÉTODOS
-	//
-	public CtrlIncluirDepartamento(CtrlAbstrato c) {
-		super(c);
-	}
-	
-	public void efetuar(String sigla, String nome) {
-		try {
-			this.depto = new Departamento(sigla,nome);
-			DaoDepartamento dao = new DaoDepartamento();
-			dao.adicionar(this.depto);
-			this.janela.notificar("Departamento Criado!");
-			this.encerrar();
-		} catch (ModelException e1) {
-			this.janela.notificar(e1.getMessage());
-		}
-	}
+
+    public CtrlIncluirDepartamento(CtrlAbstrato c) {
+        super(c);
+    }
+
+    @Override
+    public void efetuar(String sigla, String nome) {
+        try {
+            this.depto = new Departamento(sigla, nome);
+            DaoDepartamento dao = new DaoDepartamento();
+            dao.adicionar(this.depto);
+            this.janela.notificar("Departamento Criado!");
+            this.encerrar();
+        } catch (ModelException e1) {
+            this.janela.notificar(e1.getMessage());
+        }
+    }
 }
