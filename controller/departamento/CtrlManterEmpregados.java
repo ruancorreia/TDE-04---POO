9edc159300ad1525b2ea939b1f76f1a9d51da582
Ruan Correia <correia.ruan@gmail.com>
2025-09-22 17:02:23 -0300

    Update CtrlManterEmpregados.java

--- controller/departamento/CtrlManterEmpregados.java
+++ controller/departamento/CtrlManterEmpregados.java
@@ -7,54 +7,54 @@ import model.dao.DaoEmpregado;
 import viewer.JanelaConsultarEmpregados;
 
 public class CtrlManterEmpregados extends CtrlAbstrato {
-	private JanelaConsultarEmpregados janela;
-	private CtrlAbstratoEmpregado ctrlEmpregado;
+    private JanelaConsultarEmpregados janela;
+    private CtrlAbstratoEmpregado ctrlEmpregado;
 
-	public CtrlManterEmpregados(CtrlAbstrato ctrlPai) {
-		super(ctrlPai);
-		DaoEmpregado dao = new DaoEmpregado();
-		Empregado[] conjEmpregados = dao.obterTodos();
-		this.janela = new JanelaConsultarEmpregados(this, conjEmpregados);
-		this.janela.setVisible(true);
-	}
+    public CtrlManterEmpregados(CtrlAbstrato ctrlPai) {
+        super(ctrlPai);
+        DaoEmpregado dao = new DaoEmpregado();
+        Empregado[] conjEmpregados = dao.obterTodos();
+        this.janela = new JanelaConsultarEmpregados(this, conjEmpregados);
+        this.janela.setVisible(true);
+    }
 
-	public void iniciarIncluirEmpregado() {
-		this.ctrlEmpregado = new CtrlIncluirEmpregado(this);
-	}
+    public void iniciarIncluirEmpregado() {
+        this.ctrlEmpregado = new CtrlIncluirEmpregado(this);
+    }
 
-	public void iniciarAlterarEmpregado(Empregado empregadoSelecionado) {
-		if (empregadoSelecionado == null) {
-			this.janela.notificar("Selecione um empregado para alteração");
-		} else {
-			this.ctrlEmpregado = new CtrlAlterarEmpregado(this, empregadoSelecionado);
-		}
-	}
+    public void iniciarAlterarEmpregado(Empregado empregadoSelecionado) {
+        if (empregadoSelecionado == null) {
+            this.janela.notificar("Selecione um empregado para alteração");
+        } else {
+            this.ctrlEmpregado = new CtrlAlterarEmpregado(this, empregadoSelecionado);
+        }
+    }
 
-	public void iniciarExcluirEmpregado(Empregado empregadoSelecionado) {
-		if (empregadoSelecionado == null) {
-			this.janela.notificar("Selecione um empregado para exclusão");
-		} else {
-			this.ctrlEmpregado = new CtrlExcluirEmpregado(this, empregadoSelecionado);
-		}
-	}
+    public void iniciarExcluirEmpregado(Empregado empregadoSelecionado) {
+        if (empregadoSelecionado == null) {
+            this.janela.notificar("Selecione um empregado para exclusão");
+        } else {
+            this.ctrlEmpregado = new CtrlExcluirEmpregado(this, empregadoSelecionado);
+        }
+    }
 
-	public void fimEditarEmpregado() {
-		this.ctrlEmpregado = null;
-		DaoEmpregado dao = new DaoEmpregado();
-		Empregado[] conjEmpregados = dao.obterTodos();
-		this.janela.atualizarDados(conjEmpregados);
-	}
+    public void fimEditarEmpregado() {
+        this.ctrlEmpregado = null;
+        DaoEmpregado dao = new DaoEmpregado();
+        Empregado[] conjEmpregados = dao.obterTodos();
+        this.janela.atualizarDados(conjEmpregados);
+    }
 
-	@Override
-	public void encerrar() {
-		this.janela.fechar();
-		CtrlPrograma ctrl = (CtrlPrograma) getCtrlPai();
-		ctrl.manterEmpregadosFinalizado();
-	}
+    @Override
+    public void encerrar() {
+        this.janela.fechar();
+        CtrlPrograma ctrl = (CtrlPrograma) getCtrlPai();
+        ctrl.manterEmpregadosFinalizado();
+    }
 
-	@Override
-	public Object getBemTangivel() {
-		DaoEmpregado dao = new DaoEmpregado();
-		return dao.obterTodos();
-	}
+    @Override
+    public Object getBemTangivel() {
+        DaoEmpregado dao = new DaoEmpregado();
+        return dao.obterTodos();
+    }
 }
